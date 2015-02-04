90c17d53f34ceac18065ac9de88d765c1e547d6a
igithu <asy5178@163.com>
2015-02-04 16:51:09 +0800

    fix the note info

--- name_server/dist_manager/distribution_manager.cpp
+++ name_server/dist_manager/distribution_manager.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  * 
- * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * Copyright (c) 2015 aishuyu, Inc. All Rights Reserved
  * 
  **************************************************************************/
  
@@ -8,7 +8,7 @@
  
 /**
  * @file distribution_manager.cpp
- * @author aishuyu(com@baidu.com)
+ * @author aishuyu(asy5178@163.com)
  * @date 2015/02/04 10:36:34
  * @brief 
  *  
