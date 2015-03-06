f96ebe84eb032f44b4c83fab8c17a78c1c032a38
igithu <asy5178@163.com>
2015-03-06 12:11:09 +0800

    add unit test info upadte

--- src/rpc/test/testsocket_server.cpp
+++ src/rpc/test/testsocket_server.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
  * 
- * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * Copyright (c) 2015 aishuyu, Inc. All Rights Reserved
  * 
  **************************************************************************/
  
@@ -8,7 +8,7 @@
  
 /**
  * @file testsocket_server.cpp
- * @author aishuyu(com@baidu.com)
+ * @author aishuyu(asy5178@163.com)
  * @date 2015/03/05 18:35:20
  * @brief 
  *  
